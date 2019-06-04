8a717396038784c449016d78263b7c6db9fbbd02
Daniel Lindh <p.j.d.lindh@uva.nl>
2019-06-04 11:45:30 +0200

    Add files via upload

--- code/predict_ABM.py
+++ code/predict_ABM.py
@@ -7,13 +7,23 @@ from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import cross_val_predict
 from sklearn import preprocessing
 from multiprocessing import Pool
-from sklearn.feature_selection import VarianceThreshold
+from sklearn.feature_selection import VarianceThreshold, SelectFromModel
+import warnings
+from tqdm import tqdm
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 
 def predict_save(layer):
     """
     Function that predicts the ABM from layer unit activation
     """
+    layerdims = {
+    'conv1': (48, 96, 55*55),
+    'conv2': (48, 256, 27*27),
+    'conv3': (48, 384, 13*13),
+    'conv4': (48, 384, 13*13),
+    'conv5': (48, 256, 13*13)}
+
     print(f'Starting predictions for layer {layer}!')
     DCNN_PATH = '../DCNN_features'
     DATA_PATH = '../data'
@@ -24,23 +34,26 @@ def predict_save(layer):
 
     # set up pipeline with scaler, feature selection and linear regression
     lr = linear_model.LinearRegression()
-    clf = make_pipeline(preprocessing.StandardScaler(), VarianceThreshold(threshold=(.8 * (1 - .8))), lr)
-
+    clf = make_pipeline(preprocessing.StandardScaler(), SelectFromModel(lr), lr)
+    clf = make_pipeline(VarianceThreshold(threshold=0.0), preprocessing.StandardScaler(), lr)
 
     # Load layer data
     X = np.load(os.path.join(DCNN_PATH, 'features_{0}.npy'.format(layer)))
+    if 'conv' in layer:
+        laydims = layerdims[layer]
+        X = X.reshape(laydims).mean(2)
 
     preds = np.zeros((ns, X.shape[0]))
-    for sub in range(ns):
+    for sub in tqdm(range(ns)):
         #print_progress(sub, range(ns))
         preds[sub, :] = cross_val_predict(clf, X, AB[sub, :], cv=ni)
 
-    np.save(os.path.join(DATA_PATH, f'{layer}_prediction.npy'), preds)
+    np.save(os.path.join(DATA_PATH, f'test_{layer}_prediction.npy'), preds)
     print(f'Layer {layer} done!')
 
 
 
 if __name__ == '__main__':
-    layers = ('conv1', 'conv2', 'conv3', 'conv4', 'conv5', 'fc6', 'fc7', 'fc8', 'GloVe')
+    layers = ('conv1', 'conv2', 'conv3', 'conv4', 'conv5', 'fc6', 'fc7', 'fc8')
     for layer in layers:
         predict_save(layer)
--- code/predict_ABM_PERMUTATIONS.py
+++ code/predict_ABM_PERMUTATIONS.py
@@ -8,7 +8,10 @@ from sklearn import preprocessing
 import sys
 import random
 from sklearn.feature_selection import VarianceThreshold
-
+from sklearn.feature_selection import SelectFromModel
+from sklearn.svm import SVR
+import warnings
+warnings.filterwarnings("ignore")
 """
 Shuffles the labels and fits the regression for ABM for each layer and subject
 
@@ -22,7 +25,14 @@ def print_progress(i, n):
     n_bars = int(state/10)
     print('-'*n_bars, '{0:.2f}% done!'.format(state), end='\r')
 
-
+# These are the layer dimensions of the convolutional layers in AlexNet
+# (n images x n features x n rows x n columns)
+layerdims = {
+    'conv1': (48, 96, 55*55),
+    'conv2': (48, 256, 27*27),
+    'conv3': (48, 384, 13*13),
+    'conv4': (48, 384, 13*13),
+    'conv5': (48, 256, 13*13)}
 
 # Set paths and load AB data
 DCNN_PATH = '../DCNN_features'
@@ -33,7 +43,7 @@ AB = np.loadtxt(os.path.join(DATA_PATH, 'ABmag_allsubs.txt'))
 ns = AB.shape[0] # n subjects
 ni = AB.shape[1] # n images
 
-# lets do 3000 permutations
+# lets do 5000 permutations
 nperm = 3000
 
 # Make sure that every layer get the same permutation by
@@ -41,23 +51,28 @@ nperm = 3000
 imi = np.array([random.sample(range(0, ni), ni) for x in range(nperm)])
 
 layers = ['conv1', 'conv2', 'conv3', 'conv4', 'conv5', 'fc6', 'fc7', 'fc8']
+layers = [ 'fc6', 'fc7', 'fc8']
 # User input for now
-layers = [sys.argv[1]]
+#layers = [sys.argv[1]]
 
 # define our pipeline (standardscaler, feature selection, and linear regression)
 lr = linear_model.LinearRegression()
-clf = make_pipeline(preprocessing.StandardScaler(), VarianceThreshold(threshold=(0.15)), lr)
+clf = make_pipeline(VarianceThreshold(), preprocessing.StandardScaler(), lr)
 
 for layer in layers:
     print(f'Running layer: {layer}')
     predicted = np.zeros([nperm, ns, ni])
     X = np.load(os.path.join(DCNN_PATH, f'features_{layer}.npy'))
 
+    # we want to average over the feature dimension for convolutional layers
+    if 'conv' in layer:
+        laydims = layerdims[layer]
+        X = X.reshape(laydims).mean(2)
     for perm in range(nperm):
         # get the permuted
         X_ = X[imi[perm,:], :]
         print_progress(perm, nperm)
         for sub in range(ns):
-            predicted[perm, sub, :] = cross_val_predict(clf, X_, AB[sub, :], cv=48)
+            predicted[perm, sub, :] = cross_val_predict(clf, X_, AB[sub, :], cv=48, n_jobs=2)
 
     np.save(os.path.join(DATA_PATH, f'{layer}_prediction_permutations.npy'), predicted)
